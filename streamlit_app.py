0753f753634aef62a945970e91c604cda33bc2d0
Marc Minnee <marc.minnee@gmail.com>
2024-11-15 15:42:57 +0100

    model changed to groq's

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ else:
 
         # Generate a response using the OpenAI API.
         stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
+            model="mixtral-8x7b-32768",
             messages=[
                 {"role": m["role"], "content": m["content"]}
                 for m in st.session_state.messages
